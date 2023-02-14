68380b9de372560171b29a15d43d1ee201004ba7
MR-Addict <2750417853@qq.com>
2023-02-14 13:57:20 +0800

    redesign public index html

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import "./loadEnv";
 import "module-alias/register";
 
+import path from "path";
 import cors from "cors";
 import express from "express";
 import bodyParser from "body-parser";
@@ -11,9 +12,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({ origin: true }));
-app.use(express.static("./public"));
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/screenshot", screenshot);
 
 app.listen(3001, () => console.log("Listening on http://localhost:3001"));
+
+console.log(path.join(__dirname, "public"));
