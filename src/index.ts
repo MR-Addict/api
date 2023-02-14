9318f88f52d2a2bfced38e2586ab787bd85bc705
MR-Addict <2750417853@qq.com>
2023-02-14 17:44:35 +0800

    update html

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({ origin: true }));
-app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(process.cwd(), "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/screenshot", screenshot);
 
 app.listen(3001, () => console.log("Listening on http://localhost:3001"));
-
-console.log(path.join(__dirname, "public"));
