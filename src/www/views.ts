cd878ce424493b37e478cdb78785a818dca0e471
MR-Addict <2750417853@qq.com>
2023-02-22 22:11:18 +0800

    update pug

--- src/www/views.ts
+++ src/www/views.ts
@@ -1,9 +1,21 @@
 import express, { Request, Response } from "express";
 
 const router = express.Router();
+const apis = [
+  {
+    name: "Screenshot API",
+    intro: "Take any website screenshot from url! Take screenshot easier!",
+    url: "https://mraddict.one/tools/capture-website",
+  },
+  {
+    name: "Daily quote API",
+    intro: "Find beautiful quotes and improve english daily!",
+    url: "https://github.com/MR-Addict/api",
+  },
+];
 
 router.get("/", async (req: Request, res: Response) => {
-  return res.render("index");
+  return res.render("index", { apis });
 });
 
 export default router;
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["src/**/*.pug"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
